e210efcc1c1b8fe46ea0661dc0d40c6f3f2a7022
Omar Shahwan <oshahwan21@gmail.com>
2025-09-07 21:07:48 -0400

    fixed password length to 12, completed S6

--- InCollege.cob
+++ InCollege.cob
@@ -22,19 +22,19 @@
        FD ACCOUNTS-FILE.
        01 ACCOUNT-RECORD.
            05 ACCOUNT-USERNAME PIC X(20).
-           05 ACCOUNT-PASSWORD PIC X(20).
+           05 ACCOUNT-PASSWORD PIC X(12).
        
        WORKING-STORAGE SECTION.
        01 WS-EOF-FLAG PIC X VALUE 'N'.
        01 WS-ACCOUNTS-EOF PIC X VALUE 'N'.
        01 WS-USER-CHOICE PIC 9.
        01 WS-USERNAME PIC X(20).
-       01 WS-PASSWORD PIC X(20).
+       01 WS-PASSWORD PIC X(12).
        01 WS-ACCOUNT-COUNT PIC 9(2) VALUE 0.
        01 WS-LOGIN-SUCCESS PIC X VALUE 'N'.
        01 WS-TEMP-INPUT PIC X(80).
        01 WS-STORED-USERNAME PIC X(20).
-       01 WS-STORED-PASSWORD PIC X(20).
+       01 WS-STORED-PASSWORD PIC X(12).
        
        PROCEDURE DIVISION.
        MAIN-LOGIC.
@@ -96,12 +96,42 @@
                IF WS-LOGIN-SUCCESS = 'Y'
                    DISPLAY "You have successfully logged in."
                    PERFORM WRITE-TO-OUTPUT
+                   PERFORM POST-LOGIN-MENU
                ELSE
-                   DISPLAY "Incorrect username/password, please "
-      -                "try again"
+                   DISPLAY "Incorrect username/password, try again."
                    PERFORM WRITE-TO-OUTPUT
                END-IF
            END-IF.
+		
+       POST-LOGIN-MENU.
+           DISPLAY "Search for a job"
+           PERFORM WRITE-TO-OUTPUT
+           DISPLAY "Find someone you know"
+           PERFORM WRITE-TO-OUTPUT
+           DISPLAY "Learn a new skill"
+           PERFORM WRITE-TO-OUTPUT
+           DISPLAY "Enter your choice:"
+           PERFORM WRITE-TO-OUTPUT
+           
+           READ INPUT-FILE INTO WS-TEMP-INPUT
+               AT END MOVE 'Y' TO WS-EOF-FLAG
+               NOT AT END
+                   MOVE WS-TEMP-INPUT(1:1) TO WS-USER-CHOICE
+                   EVALUATE WS-USER-CHOICE
+                       WHEN 1
+                           DISPLAY "Job search is under construction."
+                           PERFORM WRITE-TO-OUTPUT
+                       WHEN 2
+                           DISPLAY "Find someone is under construction."
+                           PERFORM WRITE-TO-OUTPUT
+                       WHEN 3
+                           DISPLAY "Learn skill is under construction."
+                           PERFORM WRITE-TO-OUTPUT
+                       WHEN OTHER
+                           DISPLAY "Invalid choice, please try again"
+                           PERFORM WRITE-TO-OUTPUT
+                   END-EVALUATE
+           END-READ.
        
        VALIDATE-LOGIN.
            MOVE 'N' TO WS-LOGIN-SUCCESS
